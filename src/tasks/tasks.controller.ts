cec8bda1b017442092c5e7d31738c5b836342960
Ankit9835 <ankydchrismatic@gmail.com>
2024-02-04 22:39:47 +0530

    setting up typeorm and entity

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,41 +1,54 @@
-import { Body, Controller, Delete, Get, Param, Patch, Post, Query } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+  Query,
+} from '@nestjs/common';
 import { TasksService } from './tasks.service';
 import { Task, TaskStatus } from './task.model';
 import { title } from 'process';
 import { CreateTaskDto } from './dto/create-task-dto';
 import { GetTaskFilterDto } from './dto/get-task-filter-dto';
+import { UpdateTaskStatusDto } from './dto/update-task-status.dto';
 
 @Controller('tasks')
 export class TasksController {
-  constructor(private tasksService: TasksService) { }
+  constructor(private tasksService: TasksService) {}
 
   @Get()
-  getTasks(@Query() filterDto:GetTaskFilterDto): Task[] {
-    if(Object.keys(filterDto).length){
-      return this.tasksService.getTaskWithFilter(filterDto)
+  getTasks(@Query() filterDto: GetTaskFilterDto): Task[] {
+    if (Object.keys(filterDto).length) {
+      return this.tasksService.getTaskWithFilter(filterDto);
     } else {
-      return this.tasksService.getAllTasks()
+      return this.tasksService.getAllTasks();
     }
-    
   }
 
   @Get('/:id')
   getTaskById(@Param('id') id: string): Task {
-    return this.tasksService.getTaskById(id)
+    return this.tasksService.getTaskById(id);
   }
 
   @Delete('/:id')
   delTaskById(@Param('id') id: string): void {
-    return this.tasksService.delTaskById(id)
+    return this.tasksService.delTaskById(id);
   }
 
   @Post()
   createTask(@Body() createTaskDto: CreateTaskDto): Task {
-    return this.tasksService.createTask(createTaskDto)
+    return this.tasksService.createTask(createTaskDto);
   }
 
   @Patch('/:id/status')
-  updateTask(@Param('id') id:string, @Body('status') status:TaskStatus): Task{
-    return this.tasksService.updateTask(id, status)
+  updateTask(
+    @Param('id') id: string,
+    @Body() updateTaskDto: UpdateTaskStatusDto,
+  ): Task {
+    const { status } = updateTaskDto;
+    return this.tasksService.updateTask(id, status);
   }
 }
--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { Task, TaskStatus } from './task.model';
 import { v4 as uuidv4 } from 'uuid';
 import { CreateTaskDto } from './dto/create-task-dto';
@@ -50,7 +50,12 @@ export class TasksService {
     }
 
     getTaskById(id: string): Task {
-        return this.tasks.find((task) => task.id === id)
+        const task = this.tasks.find((task) => task.id === id)
+        if(!task) {
+            throw new NotFoundException(`Not found with the given ${id}`)
+        }
+
+        return task
     }
 
     delTaskById(id: string): void {
